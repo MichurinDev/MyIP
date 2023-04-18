66a5bd05b721727d6ba377e071c3b730d68adc1a
Michurin Andrey <andreymithurin00@gmail.com>
2023-04-18 08:39:08 +0500

    - Добавлены директории ресурсов - Добавлено новое заначение - Добавлна кнопка-ссылка на GitHub разработчика

--- WTFProj/Form1.Designer.cs
+++ WTFProj/Form1.Designer.cs
@@ -34,11 +34,15 @@
             this.IPv6_lbl = new System.Windows.Forms.Label();
             this.HintIPv4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.GH_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username_lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // IPv4_lbl
             // 
             resources.ApplyResources(this.IPv4_lbl, "IPv4_lbl");
+            this.IPv4_lbl.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.IPv4_lbl.Name = "IPv4_lbl";
             // 
             // CopyBtn
@@ -52,6 +56,7 @@
             // IPv6_lbl
             // 
             resources.ApplyResources(this.IPv6_lbl, "IPv6_lbl");
+            this.IPv6_lbl.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.IPv6_lbl.Name = "IPv6_lbl";
             // 
             // HintIPv4
@@ -64,11 +69,33 @@
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
+            // GH_btn
+            // 
+            this.GH_btn.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.GH_btn, "GH_btn");
+            this.GH_btn.Name = "GH_btn";
+            this.GH_btn.UseVisualStyleBackColor = false;
+            this.GH_btn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Username_lbl
+            // 
+            resources.ApplyResources(this.Username_lbl, "Username_lbl");
+            this.Username_lbl.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Username_lbl.Name = "Username_lbl";
+            // 
             // MainForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Username_lbl);
+            this.Controls.Add(this.GH_btn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.HintIPv4);
             this.Controls.Add(this.IPv6_lbl);
@@ -89,12 +116,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label IPv4_lbl;
         private System.Windows.Forms.Button CopyBtn;
-        private System.Windows.Forms.Label IPv6_lbl;
         private System.Windows.Forms.Label HintIPv4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GH_btn;
+        private System.Windows.Forms.Label IPv4_lbl;
+        private System.Windows.Forms.Label IPv6_lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Username_lbl;
     }
 }
 
--- WTFProj/Form1.cs
+++ WTFProj/Form1.cs
@@ -1,34 +1,50 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WTFProj
 {
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			InitializeComponent();
-		}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			// Получение имени компьютера
-			String host = System.Net.Dns.GetHostName();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Получение имени компьютера
+            String host = System.Net.Dns.GetHostName();
 
             // Получение IPv4 и IPv6
             System.Net.IPAddress ipv6 = System.Net.Dns.GetHostEntry(host).AddressList[0];
             System.Net.IPAddress ipv4 = System.Net.Dns.GetHostEntry(host).AddressList[1];
 
             // Отображаем в label'ах
-			IPv6_lbl.Text = ipv6.ToString();
+            Username_lbl.Text = host;
+            IPv6_lbl.Text = ipv6.ToString();
             IPv4_lbl.Text = ipv4.ToString();
-		}
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			// Копирует значение
-			Clipboard.SetText("IPv4: " + IPv4_lbl.Text + "\n" + "IPv6: " + IPv6_lbl.Text);
-			MessageBox.Show("Values is Copy!", "Complited", MessageBoxButtons.OK, MessageBoxIcon.Information);
-		}
-	}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Копируем значение
+            Clipboard.SetText(
+                "Username: " + Username_lbl.Text + "\n" + 
+                "IPv4: " + IPv4_lbl.Text + "\n" +
+                "IPv6: " + IPv6_lbl.Text);
+
+            // Выводим уведомление об успешном копировании
+            MessageBox.Show(
+                "Values is Copy!", "Complited",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Открывает ссылку на GitHub разработчика
+            ProcessStartInfo GH_link = new ProcessStartInfo("https://github.com/MichurinDev");
+            Process.Start(GH_link);
+        }
+    }
 }
